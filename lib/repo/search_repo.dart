7347f02203a66bc7532b1d191a34d81d1de6a214
3li-3bdullah <PROGRMMER7@gmail.com>
2023-01-21 18:37:51 +0200

    Added

---
+++ lib/repo/search_repo.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
+import 'package:gp/model/inventroy_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+import '../shared/api_status.dart';
+import '../shared/constants.dart';
+
+class SearchRepo {
+  static List<Map<String,dynamic>> listOfData = [];
+
+  static Future<List<Map<String,dynamic>>> getData(
+      {required String apiPath,required String nameAtapi ,required String? itemName}) async {
+    listOfData.clear();
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      http.Response response = await http
+          .get(Uri.http(baseUrl, apiPath, {nameAtapi: itemName}), headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ${prefs.getString('token')}'
+      });
+      if (response.statusCode == 201 || response.statusCode == 200) {
+        var body = json.decode(response.body);
+        for (var i = 0; i < body['data'].length; i++) {
+          listOfData.add(body['data'][i]);
+        }
+        return listOfData;
+      }
+      ApiStatus.checkStatus(response);
+      return listOfData;
+    } catch (e) {
+      Get.defaultDialog(title: 'Oops!', middleText: e.toString());
+      return listOfData;
+    }
+  }
+}
